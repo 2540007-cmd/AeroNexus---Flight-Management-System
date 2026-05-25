a3582ffb2061a7df580b64a30577365aeb7a80a2
Abeer Hammad <2540007@students.au.edu.pk>
2026-05-26 03:53:23 +0500

    Add RecordDB class for flight record management

---
+++ RecordDB.h
@@ -0,0 +1,30 @@
+#ifndef RECORDDB_H
+#define RECORDDB_H
+
+#include "Database.h"
+#include <string>
+using namespace std;
+
+class RecordDB : public Database {
+    protected:
+        string* airport;        // Starting airport
+        string* destination;    // Destination airport
+
+        string* modelno;     // Plane model number used in this flight
+
+        float* distance;        // Flight distance
+        float* fuelConsumed;    // Fuel consumed during flight
+
+        string* status;         // Scheduled, Completed, Cancelled
+
+    public:
+        RecordDB(string fname);
+        void loadData() override;
+        void saveData() override;
+        void addRecord() override;
+        void displayRecords() override;
+        void updateRecord() override;
+        void deleteRecord() override;
+};
+
+#endif
