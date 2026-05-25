ac00df3850a391569b1da7fb00bdf4f08da690f3
Abeer Hammad <2540007@students.au.edu.pk>
2026-05-26 03:49:48 +0500

    Update FlightDB constructor to handle file input
    Refactor FlightDB constructor to initialize arrays and set capacity based on file content.

--- OOP Project/FlightDB.cpp
+++ OOP Project/FlightDB.cpp
@@ -1,18 +1,13 @@
 #include "FlightDB.h"
+#include <iostream>
 using namespace std;
 
-FlightDB::FlightDB() {}
-
-void FlightDB::addRecord(const string& record) {
-    records.push_back(record);
-}
-
-void FlightDB::removeRecord(const string& record) {
-    // Simple implementation, remove first occurrence
-    for (auto it = records.begin(); it != records.end(); ++it) {
-        if (*it == record) {
-            records.erase(it);
-            break;
-        }
-    }
+FlightDB::FlightDB(string fname) : Database(fname){
+    setCap(); // Set capacity based on file content
+    airport = new string[capacity];
+    destination = new string[capacity];
+    modelno = new string[capacity];
+    distance = new float[capacity];
+    fuelConsumed = new float[capacity];
+    status = new string[capacity];
 }
