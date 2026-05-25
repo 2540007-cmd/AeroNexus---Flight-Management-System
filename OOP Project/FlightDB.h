36d472beeb47ded58bc63c0ce3e247077565efdb
Abeer Hammad <2540007@students.au.edu.pk>
2026-05-26 03:50:22 +0500

    Refactor FlightDB class with new member variables and methods

--- OOP Project/FlightDB.h
+++ OOP Project/FlightDB.h
@@ -5,14 +5,25 @@
 #include <string>
 using namespace std;
 
-class FlightDB : public Database {
-private:
-    // Additional members specific to flights
-public:
-    FlightDB();
-    void addRecord(const string& record) override;
-    void removeRecord(const string& record) override;
-    // Additional methods for flight management
+class FlightDB : public Database{
+    protected:
+        string* airport;        // Starting airport
+        string* destination;    // Destination airport
+
+        string* modelno;     // Plane model number used in this flight
+
+        float* distance;        // Flight distance
+        float* fuelConsumed;    // Fuel consumed during flight
+
+        string* status;         // Scheduled only
+    public:
+        FlightDB(string fname);
+        void loadData() override;
+        void saveData() override;
+        void addRecord() override;
+        void displayRecords() override;
+        void updateRecord() override;
+        void deleteRecord() override;
 };
 
-#endif // FLIGHTDB_H
+#endif
