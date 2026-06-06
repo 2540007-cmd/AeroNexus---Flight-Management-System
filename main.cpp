c052a706e0d352585e5c8fa4141cae75085e31ae
Abeer Hammad <2540007@students.au.edu.pk>
2026-06-07 03:04:03 +0500

    Company Menu Added

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <sstream>
 #include "RecordDB.h"
 #include "User.h"
+#include "Company.h"
 
 using namespace std;
 
 int main() {
-    RecordDB records("records.csv");
     User profile;
-    string fileName = "User.csv";
+    string fileName = "Data_Dependancy/Users.csv";
     bool login = false;
 
     int choice;
@@ -67,45 +67,82 @@ int main() {
         }
     } while((choice != 1 && choice != 2) || (!login)); // Loop until valid input is received or Login is successful
 
+
+    // After successful login
+
+    Company companies("Data_Dependancy/Companies.csv");
+
     do
     {
-        cout << "\n========== RecordDB Test Menu ==========" << endl;
-        cout << "1. Display Records" << endl;
-        cout << "2. Add Record" << endl;
-        cout << "3. Save Data" << endl;
-        cout << "4. Reload Data" << endl;
-        cout << "0. Exit" << endl;
+        cout << "\n========== CompanyDB Test Menu ==========\n";
+        cout << "1. Display Companies\n";
+        cout << "2. Add Company\n";
+        cout << "3. Save Companies\n";
+        cout << "4. Reload Companies\n";
+        cout << "5. Display Company's Past Records\n";
+        cout << "6. Tweak a Company's Records\n";
+        cout << "0. Exit\n";
         cout << "Enter your choice: ";
+
         cin >> choice;
         cin.ignore();
 
-        switch (choice)
+        switch(choice)
         {
-        case 1:
-            records.displayRecords();
-            break;
+            case 1:
+                companies.displayRecords();
+                break;
 
-        case 2:
-            records.addRecord();
-            break;
+            case 2:
+                companies.addRecord();
+                break;
 
-        case 3:
-            records.saveData();
-            break;
+            case 3:
+                companies.saveData();
+                break;
 
-        case 4:
-            records.loadData();
-            break;
+            case 4:
+                companies.loadData();
+                break;
 
-        case 0:
-            cout << "Exiting program..." << endl;
-            break;
+            case 5:
+            {
+                int index;
 
-        default:
-            cout << "Invalid choice. Try again." << endl;
+                companies.displayRecords();
+
+                cout << "\nEnter Company Index: ";
+                cin >> index;
+                cin.ignore();
+
+                companies.displayPastRecords(index - 1); // Display past records for the specified company index
+
+                break;
+            }
+            case 6:
+            {
+                int companyIndex;
+
+                companies.displayRecords();
+
+                cout << "\nSelect Company Index: ";
+                cin >> companyIndex;
+                cin.ignore();
+
+                companies.tweakRecords(companyIndex - 1);
+
+                break;
+            }
+
+            case 0:
+                cout << "Exiting program..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice." << endl;
         }
 
-    } while (choice != 0);
+    } while(choice != 0);
 
     return 0;
 }
